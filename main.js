34bffa8abeb70b146f19304d5bc6e4134bc2278e
Artem Yashchuk <tomkaashu@gmail.com>
2025-11-01 13:27:38 +0200

    day 4

--- main.js
+++ main.js
@@ -8,16 +8,49 @@ async function getProducts() {
     return products;
 }
 
-function getCardHTML(product){
+function getCardHTML(product) {
     return `<div class="card" style="width: 18rem;">
-  <img src="/${product.image}" class="card-img-top" alt="...">
+  <img src="${product.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.description}</p>
-    <a href="#" class="btn btn-primary">Купити</a>
+    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(product)}'>Купити</a>
   </div>
 </div>`
 }
+
+class ShoppingCart {
+    constructor() {
+        this.items = {} // об’єкт з товарами у кошику
+        this.total = 0  // загальна вартість замовлення
+    }
+}
+
+    addItem(item) { // Додавання товару до кошика 
+    if(this.items[item.title])
+    {this.items[item.title].quantity +=1
+
+    } else{this.items[item.title]=item
+        this.items[item.title].quantity=1  }
+    	
+    saveCartToCookies() { // збереження кошика у кукі
+    }
+
+    loadCartFromCookies() { // Завантаження кошика з кукі
+    }
+}
+let cart = new ShoppingCart() // Створення об'єкта кошика
+
+
+function addToCart(event) {
+    // Отримуємо дані про товар з data-атрибута кнопки 
+    const productData = event.target.getAttribute('data-product')
+    const product = JSON.parse(productData) // перетворюємо JSON у об’єкт 
+    // Тут будемо додавати товар до кошика 
+    cart.addItem(product)
+}
+
+
 
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
 getProducts().then(function (products) {
